bd24881ec9e09d86fb09212c5a6d28263bd3adae
alexx617 <290477385@qq.com>
2018-01-16 18:27:16 +0800

    add

---
+++ src/components/demo1.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+const log = console.log;
+export default {
+    data() {
+        return {
+
+        };
+    },
+    components: {},
+    created() {
+        function sleep(second) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(' enough sleep~');
+                }, second);
+            })
+        }
+        function normalFunc() {
+            console.log('normalFunc');
+        }
+        async function awaitDemo() {
+            await normalFunc();
+            console.log('something, ~~');
+            let result = await sleep(2000);
+            console.log(result);// 两秒之后会被打印出来
+        }
+        awaitDemo();
+    },
+    methods: {
+        // async awaitDemo(){
+        //     await normalFunc();
+        //     console.log('2')
+        //     let time = await sleep(2000);
+        //     console.log(time)
+        // }
+    },
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 // import home from '@/components/home'
 const home = r => require.ensure([], () => r(require('components/home')), 'home')
 const demo = r => require.ensure([], () => r(require('components/demo')), 'home')
+const demo1 = r => require.ensure([], () => r(require('components/demo1')), 'home')
 const form = r => require.ensure([], () => r(require('components/form/form')), 'home')
 const one = r => require.ensure([], () => r(require('components/form/one')), 'home')
 const mask = r => require.ensure([], () => r(require('components/mask')), 'home')
@@ -14,18 +15,20 @@ Vue.use(VueRouter)
 const routes = [
 	{ path: '/home', name: 'home', component: home },
 	{ path: '/demo', name: 'demo', component: demo },
+	{ path: '/', name: 'demo1', component: demo1 },
 	{ path: '/form', name: 'form', component: form },
 	{ path: '/mask', name: 'mask', component: mask },
 	{ path: '/mask_t', name: 'mask_t', component: mask_t },
 	{ path: '/pic', name: 'pic', component: pic },
-	{ path: '/', name: 'one', component: one },
+	{ path: '/one', name: 'one', component: one },
 	{ path: '*', redirect: '/' }
 ]
 
 const router = new VueRouter({ routes });
 
-router.afterEach(() => {
+router.beforeEach((to, from, next) => {
     document.body.scrollTop = 0;
+	next();
 });
 
 export default router;
